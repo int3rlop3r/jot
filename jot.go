f2e8a666fa680cc985587f7157f696f1f12a6a4f
jonathan <jonathan.fernandes@netcore.co.in>
2016-11-14 11:01:10 +0530

    Fixed: creation of dirs in the data dir while running the ls command

--- jot.go
+++ jot.go
@@ -1,69 +1,69 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "strings"
-    "path/filepath"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func procCmd(jotArgs []string) {
-    var confrm string
+	var confrm string
 
-    curdir, err := os.Getwd()
-    homedir := os.Getenv("HOME")
-    datadir := filepath.Join(homedir, ".jot")
+	curdir, err := os.Getwd()
+	homedir := os.Getenv("HOME")
+	datadir := filepath.Join(homedir, ".jot")
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    jo := &JotOps{curdir, datadir}
-    jo.Init()
+	jo := &JotOps{curdir, datadir}
+	jo.Init()
 
-    switch jotArgs[1] {
-    case "ls":
-        fmt.Println("Jot files in this folder")
-        err := listDir(jo.GetProjDir())
+	switch jotArgs[1] {
+	case "ls":
+		fmt.Println("Jot files in this folder")
+		err := jo.ListDir(jo.GetProjDir())
 
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "Error listing dir: %s", err)
-        }
-    case "clean-all":
-        fmt.Println("Delete all 'jot' files from this system?")
-        fmt.Scanln(&confrm)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "No jot files present for: %s", curdir)
+		}
+	case "clean-all":
+		fmt.Println("Delete all 'jot' files from this system?")
+		fmt.Scanln(&confrm)
 
-        if p := strings.ToLower(confrm); p != "yes" && p != "y" {
-            fmt.Println("Aborted")
-            return
-        }
+		if p := strings.ToLower(confrm); p != "yes" && p != "y" {
+			fmt.Println("Aborted")
+			return
+		}
 
-        os.RemoveAll(datadir)
-        os.Mkdir(datadir, os.ModePerm)
+		os.RemoveAll(datadir)
+		os.Mkdir(datadir, os.ModePerm)
 
-        fmt.Println("Deleted all 'jot' files on this system!")
-    case "clean":
-        fmt.Println("Delete all 'jot' files in this project?")
-        fmt.Scanln(&confrm)
+		fmt.Println("Deleted all 'jot' files on this system!")
+	case "clean":
+		fmt.Println("Delete all 'jot' files in this project?")
+		fmt.Scanln(&confrm)
 
-        if p := strings.ToLower(confrm); p != "yes" && p != "y" {
-            fmt.Println("Aborted")
-            return
-        }
-        os.RemoveAll(jo.GetProjDir())
+		if p := strings.ToLower(confrm); p != "yes" && p != "y" {
+			fmt.Println("Aborted")
+			return
+		}
+		os.RemoveAll(jo.GetProjDir())
 
-        fmt.Println("Deleted all 'jot' files in this project!")
-    default:
-        absFilePath := filepath.Join(jo.GetProjDir(), jotArgs[1])
-        editFile(absFilePath)
-    }
+		fmt.Println("Deleted all 'jot' files in this project!")
+	default:
+		absFilePath := filepath.Join(jo.GetProjDir(), jotArgs[1])
+		jo.EditFile(absFilePath)
+	}
 }
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: jot [options]")
-    } else {
-        procCmd(os.Args)
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: jot [options]")
+	} else {
+		procCmd(os.Args)
+	}
 }
--- utils.go
+++ utils.go
@@ -1,109 +1,116 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
-    "path/filepath"
-    "crypto/sha1"
-    "encoding/hex"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 type JotOps struct {
-    curDir, dataDir string
+	curDir, dataDir string
 }
 
 func (jo JotOps) exists(path string) (bool, error) {
-    _, err := os.Stat(path)
-    if err == nil { return true, nil }
-    if os.IsNotExist(err) { return false, nil }
-    return true, err
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
 }
 
-func (jo JotOps) makeSha1(dirpath string)  string {
-    h := sha1.New()
-    h.Write([]byte(dirpath))
-    return hex.EncodeToString(h.Sum(nil))
+func (jo JotOps) makeSha1(dirpath string) string {
+	h := sha1.New()
+	h.Write([]byte(dirpath))
+	return hex.EncodeToString(h.Sum(nil))
 }
 
 func (jo JotOps) makeDir(dirPath string) (bool, error) {
-    dexists, err := jo.exists(dirPath)
+	dexists, err := jo.exists(dirPath)
 
-    if err != nil { return false, err }
+	if err != nil {
+		return false, err
+	}
 
-    if !dexists {
-        os.Mkdir(dirPath, os.ModePerm)
-    }
+	if !dexists {
+		os.Mkdir(dirPath, os.ModePerm)
+	}
 
-    return true, err
+	return true, err
 }
 
 func (jo JotOps) makeDataDir() {
-    _, err := jo.makeDir(jo.dataDir)
+	_, err := jo.makeDir(jo.dataDir)
 
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Coud not create data dir: %s", err)
-    }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Coud not create data dir: %s", err)
+	}
 }
 
 func (jo JotOps) makeProjDir() {
-    _, err := jo.makeDir(jo.GetProjDir())
+	_, err := jo.makeDir(jo.GetProjDir())
 
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Coud not create project dir: %s", err)
-    }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Coud not create project dir: %s", err)
+	}
 }
 
 func (jo JotOps) GetDataDir() string {
-    return jo.dataDir
+	return jo.dataDir
 }
 
 func (jo JotOps) GetProjDir() string {
-    pathHash := jo.makeSha1(jo.curDir)
-    return filepath.Join(jo.dataDir, pathHash)
+	pathHash := jo.makeSha1(jo.curDir)
+	return filepath.Join(jo.dataDir, pathHash)
 }
 
 func (jo JotOps) Init() {
 
-    // create data dir if it doesn't exist
-    jo.makeDataDir()
+	// create data dir if it doesn't exist
+	jo.makeDataDir()
 
-    // make project dir if it doesn't exist!
-    jo.makeProjDir()
 }
 
-func listDir(dirPath string) error {
-    d, err := os.Open(dirPath)
+func (jo JotOps) ListDir(dirPath string) error {
+	d, err := os.Open(dirPath)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    defer d.Close()
-    names, err := d.Readdirnames(-1)
-    if err != nil {
-        return err
-    }
-    for _, name := range names {
-        fstats, err := os.Stat(filepath.Join(dirPath, name))
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		fstats, err := os.Stat(filepath.Join(dirPath, name))
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        fmt.Println(fstats.ModTime(), fstats.Name())
-    }
+		fmt.Println(fstats.ModTime(), fstats.Name())
+	}
 
-    return err
+	return err
 }
 
-func editFile(filePath string) {
-    cmd := exec.Command("editor", filePath)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
+func (jo JotOps) EditFile(filePath string) {
+	// make project dir if it doesn't exist!
+	jo.makeProjDir()
 
-    err := cmd.Run()
-    if err != nil { // add this to the case stmt???
-        fmt.Fprintf(os.Stderr, "Coud not open file for editing: %s", err)
-    }
+	cmd := exec.Command("editor", filePath)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Run()
+	if err != nil { // add this to the case stmt???
+		fmt.Fprintf(os.Stderr, "Coud not open file for editing: %s", err)
+	}
 }
